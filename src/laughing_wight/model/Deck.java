c36ec2e717d2bd0528b433ae406a8e466a61dd4f
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2012-09-12 22:50:24 +0200

    Added a Deck of Cards.

---
+++ src/laughing_wight/model/Deck.java
@@ -0,0 +1,31 @@
+package laughing_wight.model;
+
+import java.util.Random;
+
+public class Deck {
+	private Card[] cards;
+	private int count;
+	private Random rand;
+	
+	public Deck(){
+		count = 51;
+		cards = new Card[52];
+		for(Suit suit : Suit.values()){
+			for (int i = 2; i < 14; i++) {
+				cards[suit.ordinal()*13 + i - 2] = new Card(suit,i);
+			}
+		}
+	}
+	
+	public Card draw(){
+		if(count <= 0 ){
+			throw new IllegalStateException("Drew from an empty card deck!");
+		}
+		int pick = rand.nextInt(count);
+		Card ret = cards[pick];
+		cards[pick] = cards[count];
+		count--;
+		return ret;
+	}
+
+}
